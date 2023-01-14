57088cd369b3de24e001bc9475db560b8842de65
Hans Tool <hans@defect.org>
2023-01-14 09:30:53 +0100

    Adding UUID for 12Nm util

--- update_country_territoral.js
+++ update_country_territoral.js
@@ -77,10 +77,12 @@ function handleJson(json) {
             var options = {tolerance: 0.01, highQuality: false};
             var simplified = turf.simplify(polygon, options)
             // console.log('simplified: ' + JSON.stringify(simplified.geometry.coordinates))
+            var UUID = generateUUID()
             simplified.properties = {
 					    "name": country,
 					    "callname": "",
 					    "type": "territorial",
+              "id": UUID,
 					    "vhfdata": {
 					      "generic": {
 					        "channel": 16,
@@ -189,3 +191,20 @@ function getCode (country) {
 	}
   return codes[country]
 }
+
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
